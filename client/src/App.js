741b6859d559cfede2897c3fd7ead49a2356cb79
Isaac Kang <isaac.kang@consensys.net>
2020-02-10 15:57:08 -0500

    ref: hooks -> class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-// import moment from 'moment';
 
 import './App.css';
 import { getFormattedReservations, postReservation } from './utils';
@@ -34,71 +33,81 @@ const Button = styled.button`
   margin: 1em;
 `;
 
-function App() {
-  const [reservations, setReservations] = useState({
-    booked: [],
-    available: {},
-  });
-  const [resName, setResName] = useState('');
-  const [availableHours, setAvailableHours] = useState([]);
+class App extends React.Component {
+  state = {
+    bookedReservations: [],
+    availableHours: [],
+    resName: '',
+  };
 
-  useEffect(() => {
-    getFormattedReservations().then(setReservations);
-  }, []);
+  componentDidMount() {
+    this.refreshReservations();
+  }
 
-  useEffect(() => {
-    const available = Object.keys(reservations.available).filter(
-      hour => reservations.available[hour] !== 0
-    );
+  refreshReservations = () => {
+    getFormattedReservations().then(reservations => {
+      const available = Object.keys(reservations.available).filter(
+        hour => reservations.available[hour] !== 0
+      );
 
-    setAvailableHours(available);
-  }, [reservations]);
+      this.setState({
+        bookedReservations: reservations.booked,
+        availableHours: available,
+      });
+    });
+  };
 
-  async function addReservation(timeSlot) {
-    if (resName === '' || typeof resName !== 'string') {
+  addReservation = async timeSlot => {
+    if (this.state.resName === '' || typeof this.state.resName !== 'string') {
       alert('must be string');
       return;
     }
     let day = new Date();
     day.setHours(timeSlot.slice(0, 2), timeSlot.slice(3, 5), 0, 0);
-    await postReservation(resName, day);
-    setResName('');
 
-    const formatted = await getFormattedReservations();
-    setReservations(formatted);
-  }
+    const newReservation = await postReservation(this.state.resName, day);
+    console.log('newReservation:', newReservation);
 
-  return (
-    <div className="App">
-      <div>Janky Meats</div>
+    this.setState({
+      resName: '',
+    });
 
-      <BookedReservations>
-        <div>RESERVATIONS</div>
-        {reservations.booked.map((res, ind) => (
-          <div key={res.name + res.slot + ind}>
-            {res.name} {res.slot}
-          </div>
-        ))}
-      </BookedReservations>
+    this.refreshReservations()
+  };
 
-      <ReservationForm>
-        <Input
-          type="text"
-          value={resName}
-          placeholder="Enter your name"
-          onChange={e => setResName(e.target.value)}
-        />
+  render() {
+    return (
+      <div className="App">
+        <div>Janky Meats</div>
 
-        <ButtonContainer>
-          {availableHours.map(timeSlot => (
-            <Button onClick={() => addReservation(timeSlot)}>
-              {timeSlot.slice(0, 2) - 12} {timeSlot.slice(2)} PM
-            </Button>
+        <BookedReservations>
+          <div>RESERVATIONS</div>
+          {this.state.bookedReservations.map((res, ind) => (
+            <div key={res.name + res.slot + ind}>
+              {res.name} {res.slot}
+            </div>
           ))}
-        </ButtonContainer>
-      </ReservationForm>
-    </div>
-  );
+        </BookedReservations>
+
+        <ReservationForm>
+          <Input
+            type="text"
+            value={this.state.resName}
+            placeholder="Enter your name"
+            onChange={e => this.setState({ resName: e.target.value })}
+          />
+
+          <ButtonContainer>
+            {this.state.availableHours.map(timeSlot => (
+              <Button onClick={() => this.addReservation(timeSlot)}>
+                {timeSlot.slice(0, 2) - 12} {timeSlot.slice(2)} PM
+              </Button>
+            ))}
+          </ButtonContainer>
+        </ReservationForm>
+      </div>
+    );
+  }
 }
 
 export default App;
