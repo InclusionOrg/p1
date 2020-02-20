6fc326fe397b36851e5311276a80f10ba128ced5
Isaac Kang <isaac.kang@protonmail.ch>
2020-02-20 01:14:09 +0000

    Updates

---
+++ precache-manifest.451b660e34efd611704809224ddcc0c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d211fce098fcefac02733f2b7fe289a",
+    "url": "/project-1/index.html"
+  },
+  {
+    "revision": "d083c356ed35f682ee84",
+    "url": "/project-1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "790e24cce1b374240eec",
+    "url": "/project-1/static/js/2.29ad3d23.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/project-1/static/js/2.29ad3d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d083c356ed35f682ee84",
+    "url": "/project-1/static/js/main.5cf5de8f.chunk.js"
+  },
+  {
+    "revision": "01e336b16b1f738501b5",
+    "url": "/project-1/static/js/runtime-main.60660c69.js"
+  }
+]);
